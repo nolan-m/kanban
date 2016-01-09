b896fc0639b55f9cc53bcd0d694cc09b0fa0c5d1
nolan-m <namiller2@gmail.com>
2016-01-09 15:56:10 -0800

    Add card component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './components/Card';
 
 var App = React.createClass ({
   render(){
     return (
       <div>
         <h1>Kanban</h1>
+        <Card task={'do some work'} />
       </div>
     );
   }
---
+++ components/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+var Card = React.createClass({
+  render: function(){
+    return (
+      <div style={this.styles.card}>
+        <p style={this.styles.task}>{this.props.task}</p>
+      </div>
+    );
+  },
+
+  styles: {
+    card: {
+      border: '1px solid black',
+      width: 250,
+      padding: 15,
+      background: '#F2F2F2'
+    },
+    task: {
+      fontWeight: 700,
+      margin: 5,
+      fontSize: 18
+    }
+  }
+
+});
+
+export default Card
